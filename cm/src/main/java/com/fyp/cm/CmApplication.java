323c047aad9866afe63571365295fcf3904d47eb
Zhang Xiang <17215501@siswa.um.edu.my>
2023-05-30 14:42:38 +0800

    add cross origin

--- cm/src/main/java/com/fyp/cm/CmApplication.java
+++ cm/src/main/java/com/fyp/cm/CmApplication.java
@@ -2,6 +2,9 @@ package com.fyp.cm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CmApplication {
@@ -9,5 +12,18 @@ public class CmApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CmApplication.class, args);
 	}
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:8080")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
 
 }
