cc166933d73190a68809345333e788be57f63561
zhangxiang0518 <3393906183@qq.com>
2024-01-10 14:18:40 +0800

    is activate v2

--- src/main/java/com/fyp/cm/service/UserServiceImpl.java
+++ src/main/java/com/fyp/cm/service/UserServiceImpl.java
@@ -26,7 +26,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User addUser(User user) {
-        user.setIsActive(false);
+        user.setIsActive(Boolean.FALSE);
         return userRepo.save(user);
     }
 
